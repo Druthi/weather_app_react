61d6f648f17c1645bfc23586a5112ac62a695887
Druthi Polisetty <polisettydruthi@gmail.com>
2017-12-02 14:39:56 +0530

    Inputs city name and renders weather data

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Request from 'superagent';
 class App extends Component {
   constructor() {
     super();
-    this.state={}
+    this.state={
+      city: "Bangalore"
+    }
   }
 
   componentWillMount() {
-    var url = "http://api.openweathermap.org/data/2.5/weather?APPID=cf1d3f8e4f9d884c4db508e806a67b17&q=Bangalore";
+    var url = "http://api.openweathermap.org/data/2.5/weather?APPID=cf1d3f8e4f9d884c4db508e806a67b17&q="+ this.state.city;
     Request.get(url).then((response) => {
       this.setState({
-        fWeather: (response.body.main.temp *9/5) - 273.15,
+        fWeather: (response.body.main.temp *9/5) - 459.67,
         cWeather: response.body.main.temp - 273.15,
-        currentWeather: (response.body.main.temp *9/5) - 273.15,
-        celcius: false
+        currentWeather: (response.body.main.temp *9/5) - 459.67,
+        celcius: false,
+        status: response.body.weather[0].description
       });
     });
   }
@@ -36,13 +39,48 @@ class App extends Component {
       });
     }
   }
+
+  onChange = (e) => {
+    this.setState({
+      ...this.state,
+      inputValue: e.target.value      
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ ...this.state,
+      city: this.state.inputValue,
+      inputValue: ""
+    });
+    this.apiRequest();    
+  }
+
+  apiRequest = () => {
+    var url = "http://api.openweathermap.org/data/2.5/weather?APPID=cf1d3f8e4f9d884c4db508e806a67b17&q="+ this.state.city;
+    Request.get(url).then((response) => {
+      this.setState({
+        fWeather: (response.body.main.temp *9/5) - 459.67,
+        cWeather: response.body.main.temp - 273.15,
+        currentWeather: (response.body.main.temp *9/5) - 459.67,
+        celcius: false,
+        status: response.body.weather[0].description
+      });
+    });
+  }
   
 
   render() {
     return (
       <div className="App">
         <header>Weather App</header> 
-        <p onClick={this.showWeather}>{this.state.currentWeather}</p>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.inputValue} placeholder="Enter city" onChange={this.onChange}></input>
+          <button type="submit" >Submit</button>
+        </form>
+        <p onClick={this.showWeather}>{this.state.currentWeather} </p>
+        <p>{this.state.status}</p>
+        
               
       </div>
     );
